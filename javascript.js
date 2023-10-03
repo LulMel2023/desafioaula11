3e6decdb5b26ccd6df313675fe00384c3eb2a609
Luciana Melo Aguiar <127307773+LulMel2023@users.noreply.github.com>
2023-10-03 18:33:53 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+//alert("Olá Mundo!")
+
+//var nome = prompt('Digite seu nome:');
+//alert('Seja bem-vinda,'+ nome + '!');
+
+//var n1 = parseInt(prompt('Digite o primeiro número:'));
+//var n2 = parseInt(prompt('Digite o segundo número:'));
+//alert('A soma é:' + (n1+n2));
+
+//var n1 = parseFloat(prompt('Digite a primeira nota:'));
+//var n2 = parseFloat(prompt('Digite a segunda nota:'));
+//var media = (n1+n2)/2;
+
+//alert('A média é:' + media);
+
+//if(media>=7){
+    //alert('Aprovada');
+//}else{
+    //alert('Reprovada');
+//}
+var numero = parseInt(prompt('Digite um número inteiro:'));
+
+if(numero % 2 === 0 && numero < 100) {
+    alert('O número atende aos critérios');
+}else{
+    alert('O número não atende os critérios');
+}
+
